648aaa69c271f87e537b9da6d64cd96e55efea56
lima-rafa <profissionalrfrl@gmail.com>
2023-05-31 08:46:52 -0300

    2º erro build

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, createContext} from "react";
+import React, {useState, createContext} from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
